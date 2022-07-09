393e9fc9a437cd0f9b0a1d2becbeafb27a636a5e
Marie Le Carvennec <marie.lecarvennec@gmail.com>
2022-07-10 00:47:32 +0200

    Formulaire de contact

--- includes/form_1.php
+++ includes/form_1.php
@@ -1,23 +1,23 @@
 <?php	
-	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
+	if(empty($_POST['nom']) && strlen($_POST['nom']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['info']) && strlen($_POST['info']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
 	{
 		return false;
 	}
 	
-	$name = $_POST['name'];
+	$name = $_POST['nom'];
 	$email = $_POST['email'];
-	$input_504 = $_POST['input_504'];
+	$info = $_POST['info'];
 	$message = $_POST['message'];
 	
-	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
+	$to = 'receiver@lapanthere.com'; // Email submissions are sent to this email
 
 	// Create email	
-	$email_subject = "Message from your website";
-	$email_body = "You have received a new message. \n\n".
-				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
+	$email_subject = "Message depuis le formulaire de contact de votre site La Panthère";
+	$email_body = "Vous avez reçu un nouveau message. \n\n".
+				  "Nom: $name \nEmail: $email \nInfo: $info \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
-	$headers .= "From: contact@yoursite.com\n";
-	$headers .= "Reply-To: $input_504";	
+	$headers .= "From: contact@lapanthere.com\n";
+	$headers .= "Reply-To: $info";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
 	return true;			
