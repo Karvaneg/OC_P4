1af052846fb27a21a6838bec034e62847769d956
Marie Le Carvennec <marie.lecarvennec@gmail.com>
2022-07-10 16:42:08 +0200

    Essai d'un PWA

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//if ('serviceWorker' in navigator) {
+ //   navigator.serviceWorker
+  //    .register("/sw.js")
+   //   .then(serviceWorker => {
+    //    console.log("Service Worker registered: ", serviceWorker);
+   //   })
+   //   .catch(error => {
+    //    console.error("Error registering the Service Worker: ", error);
+    //  });
+ // }
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js', { scope: '/js/' }).then(function(reg) {
+      // registration worked
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  };
