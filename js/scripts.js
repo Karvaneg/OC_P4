163f0444f7856a7559f1a35276e2b4ec84ea8cc3
Marie Le Carvennec <marie.lecarvennec@gmail.com>
2022-07-10 17:26:49 +0200

    Modification code scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,26 @@
     //    console.error("Error registering the Service Worker: ", error);
     //  });
  // }
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/OC_P4/sw.js', { scope: '/OC_P4/js/' }).then(function(reg) {
+ // if ('serviceWorker' in navigator) {
+  //  navigator.serviceWorker.register('/OC_P4/sw.js', { scope: '/OC_P4/js/' }).then(function(reg) {
       // registration worked
-      console.log('Registration succeeded. Scope is ' + reg.scope);
-    }).catch(function(error) {
+  //    console.log('Registration succeeded. Scope is ' + reg.scope);
+   // }).catch(function(error) {
       // registration failed
-      console.log('Registration failed with ' + error);
-    });
-  };
+    //  console.log('Registration failed with ' + error);
+  //  });
+ // };
+
+// Don't register the service worker
+  // until the page has fully loaded
+  window.addEventListener('load', () => {
+    // Is service worker available?
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/OC_P4/sw.js').then(() => {
+        console.log('Service worker registered!');
+      }).catch((error) => {
+        console.warn('Error registering service worker:');
+        console.warn(error);
+      });
+    }
+  });
