922f73f7cc3a2c115c65a42fa17af0477842e1e8
Marie Le Carvennec <marie.lecarvennec@gmail.com>
2022-07-10 18:46:20 +0200

    Ajout scope manifest

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,40 @@ window.addEventListener('load', () => {
     console.log('Service workers are not supported.')
   }
 })
+
+
+
+
+// Don't register the service worker
+  // until the page has fully loaded
+ // window.addEventListener('load', () => {
+    // Is service worker available?
+   // if ('serviceWorker' in navigator) {
+   //   navigator.serviceWorker.register('/OC_P4/sw.js').then(() => {
+   //     console.log('Service worker registered!');
+    //  }).catch((error) => {
+     //   console.warn('Error registering service worker:');
+      //  console.warn(error);
+    //  });
+   // }
+ // });
+
+  //if ('serviceWorker' in navigator) {
+ //   navigator.serviceWorker
+  //    .register("/sw.js")
+   //   .then(serviceWorker => {
+    //    console.log("Service Worker registered: ", serviceWorker);
+   //   })
+   //   .catch(error => {
+    //    console.error("Error registering the Service Worker: ", error);
+    //  });
+ // }
+ // if ('serviceWorker' in navigator) {
+  //  navigator.serviceWorker.register('/OC_P4/sw.js', { scope: '/OC_P4/js/' }).then(function(reg) {
+      // registration worked
+  //    console.log('Registration succeeded. Scope is ' + reg.scope);
+   // }).catch(function(error) {
+      // registration failed
+    //  console.log('Registration failed with ' + error);
+  //  });
+ // };
