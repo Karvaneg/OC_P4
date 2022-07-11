c26578fbc0a0d5875a3491bc587a5a69dabe96c3
Marie Le Carvennec <marie.lecarvennec@gmail.com>
2022-07-11 10:39:03 +0200

    CSS et JS minifiés

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,1 @@
-// Don't register the service worker
-// until the page has fully loaded
-window.addEventListener('load', () => {
-    // Is service worker available?
- if ('serviceWorker' in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker.register('/OC_P4/js/sw.js', {scope: '/OC_P4/js/'}).then(
-      (registration) => {
-        console.log('Enregistrement réussi:', registration);
-      }).catch((error) => {
-           console.log('Erreur:' + error);
-      }
-    );
-  } else {
-    console.log('Service workers non supporté.')
-  }
-})
+window.addEventListener('load',()=>{if('serviceWorker' in navigator){navigator.serviceWorker.register('/OC_P4/js/sw.js',{scope:'/OC_P4/js/'}).then((registration)=>{console.log('Enregistrement réussi:',registration)}).catch((error)=>{console.log('Erreur:'+error)})}else{console.log('Service workers non supporté.')}})
--- js/sw.js
+++ js/sw.js
@@ -1,9 +1,1 @@
-self.addEventListener("install", event => {
-    console.log("Service Worker installing.");
-  });
-  
-  self.addEventListener("activate", event => {
-    console.log("Service Worker activating.");
-  });
-
-self.addEventListener("fetch", function(event) {});
+self.addEventListener("install",event=>{console.log("Service Worker installing.")});self.addEventListener("activate",event=>{console.log("Service Worker activating.")});self.addEventListener("fetch",function(event){})
